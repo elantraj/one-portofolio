09ee731fd1174b0855162db2d9cac5b582a995c3
elantra jashari <elantrajashari@outlook.com>
2022-07-18 15:44:43 +0200

    About section- dummy text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
@@ -7,6 +8,7 @@ function App() {
  <div>
 <NavBar />
  <Home />
+ <About />
  <SocialLinks />
  </div>
   );
