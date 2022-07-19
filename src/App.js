82526ffc7b082d3e22b1c8ae129678dc28eb7305
elantra jashari <elantrajashari@outlook.com>
2022-07-19 14:37:23 +0200

    added new component -experience

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
 import SocialLinks from "./components/SocialLinks";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
  <Home />
  <About />
  <Portfolio />
+ <Experience />
  <SocialLinks />
  </div>
   );
---
+++ src/components/Experience.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import html from "../assets/html.png";
+
+const Experience = () => {
+    const techs = [
+      {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500",
+      }
+    ];
+}
+export default Experience;
