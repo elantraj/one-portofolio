e489c968a177f1eddc1e1fb5f8a16c27e78771be
elantra jashari <elantrajashari@outlook.com>
2022-07-19 15:12:56 +0200

    content to experience

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import Experience from "./components/Experience";
 import SocialLinks from "./components/SocialLinks";
+
 function App() {
   return (
  <div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import reactImage from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+import vuejs from "../assets/vuejs.png";
+import bootstraplogo from "../assets/bootstraplogo.png";
+
+
+
 
 const Experience = () => {
     const techs = [
@@ -10,6 +18,71 @@ const Experience = () => {
         title: "HTML",
         style: "shadow-orange-500",
       }
+      ,
+      {
+        id: 2,
+        src: bootstraplogo,
+        title: "Bootstrap",
+        style: "shadow-sky-400",
+      },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+        id: 6,
+        src: vuejs,
+        title: "VueJs",
+        style: "shadow-sky-400",
+      },
+   
+     
+   
     ];
-}
+
+
+    return (
+        <div
+          name="experience"
+          className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+        >
+          <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+            <div>
+              <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                Experience
+              </p>
+              <p className="py-6">These are the technologies I've worked with</p>
+            </div>
+    
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+              {techs.map(({ id, src, title, style }) => (
+                <div
+                  key={id}
+                  className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                >
+                  <img src={src} alt="" className="w-20 mx-auto" />
+                  <p className="mt-4">{title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    };
+
+
 export default Experience;
